3fcbf1088c93c37daa06e52a63f362992fbbb337
bernardobol <beboscardimlazzarotto@hotmail.com>
2022-05-16 23:42:03 -0300

    Adicionado Commit migrates, seeders e controllers

---
+++ Projeto_conclusao/database/seeds/PostsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'post_date' => '2022/05/16',
+            'category_id' => 1,
+            'title' => 'Título teste',
+            'summary' => 'Sumário teste',
+            'text' => 'Texto teste',
+            'active' => true
+        ]);
+    }
+}
